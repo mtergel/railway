4ce3a5dc9ccc4894861c9a2f87d28f673571eb14
Tergel Munkhdelger <tergelm@gmail.com>
2021-05-21 01:01:39 +0800

    added more typography

--- components/Editor/RichEditor.tsx
+++ components/Editor/RichEditor.tsx
@@ -4,7 +4,7 @@ import StarterKit from "@tiptap/starter-kit";
 import { IconButton } from "../Button";
 import { IoTextOutline } from "react-icons/io5";
 import { ImBold, ImItalic, ImStrikethrough } from "react-icons/im";
-import { useEffect } from "react";
+import { memo, useEffect } from "react";
 import clsx from "clsx";
 
 export const RichEditor = () => {
@@ -20,83 +20,122 @@ export const RichEditor = () => {
     } else {
       // new document
       if (editor) {
-        editor
-          .chain()
-          .toggleHeading({
-            level: 1,
-          })
-          .run();
+        editor.chain().focus().toggleHeading({ level: 1 }).run();
       }
     }
-  }, []);
+  }, [content]);
 
   return (
     <div className="flex flex-col min-h-0 min-w-0 h-full bg-editor">
-      <div className="px-8 py-4 bg-editor hover:shadow-md w-full transition-all hover:bg-default">
+      <div className="px-4 py-2 bg-editor hover:shadow-md w-full transition-shadow hover:bg-default">
         {editor && (
           <Popover as="div" className="relative inline-block text-left">
             <div>
               <Popover.Button
                 as={IconButton}
                 aria-label="Typography"
+                size="lg"
                 variant="ghost"
                 className="dark:text-gray-400"
               >
                 <IoTextOutline />
               </Popover.Button>
             </div>
-            <Transition
-              enter="transition duration-100 ease-out"
-              enterFrom="transform scale-95 opacity-0"
-              enterTo="transform scale-100 opacity-100"
-              leave="transition duration-75 ease-out"
-              leaveFrom="transform scale-100 opacity-100"
-              leaveTo="transform scale-95 opacity-0"
-            >
-              <Popover.Panel className="absolute z-50 left-0 w-32 mt-2 origin-top-right bg-paper divide-y divide-gray-100 dark:divide-gray-600 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <div className="p-2 flex justify-around w-full ">
-                  <IconButton
-                    variant={editor.isActive("bold") ? "solid" : "ghost"}
-                    aria-label="bold"
-                    size="sm"
-                    onClick={() => editor.chain().focus().toggleBold().run()}
-                    color={editor.isActive("bold") ? "primary" : undefined}
-                  >
-                    <ImBold />
-                  </IconButton>
-                  <IconButton
-                    variant={editor.isActive("italic") ? "solid" : "ghost"}
-                    aria-label="italic"
-                    size="sm"
-                    onClick={() => editor.chain().focus().toggleItalic().run()}
-                    color={editor.isActive("italic") ? "primary" : undefined}
-                  >
-                    <ImItalic />
-                  </IconButton>
-                  <IconButton
-                    variant={editor.isActive("strike") ? "solid" : "ghost"}
-                    aria-label="strike"
-                    size="sm"
-                    onClick={() => editor.chain().focus().toggleStrike().run()}
-                    color={editor.isActive("strike") ? "primary" : undefined}
-                  >
-                    <ImStrikethrough />
-                  </IconButton>
-                </div>
-                <button
-                  type="button"
-                  className={clsx(
-                    "p-2 text-xl text-center",
-                    editor.isActive("heading", { level: 1 }) && "text-red-300"
-                  )}
+
+            <Popover.Panel className="absolute z-50 left-0 w-48 mt-2 origin-top-right bg-paper border dark:border-gray-600 border-opacity-40 border-gray-200 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="p-2 flex justify-around w-full">
+                <IconButton
+                  variant={editor.isActive("bold") ? "solid" : "ghost"}
+                  aria-label="bold"
+                  size="sm"
+                  onClick={() => editor.chain().focus().toggleBold().run()}
+                  color={editor.isActive("bold") ? "primary" : undefined}
+                >
+                  <ImBold />
+                </IconButton>
+                <IconButton
+                  variant={editor.isActive("italic") ? "solid" : "ghost"}
+                  aria-label="italic"
+                  size="sm"
+                  onClick={() => editor.chain().focus().toggleItalic().run()}
+                  color={editor.isActive("italic") ? "primary" : undefined}
+                >
+                  <ImItalic />
+                </IconButton>
+                <IconButton
+                  variant={editor.isActive("strike") ? "solid" : "ghost"}
+                  aria-label="strike"
+                  size="sm"
+                  onClick={() => editor.chain().focus().toggleStrike().run()}
+                  color={editor.isActive("strike") ? "primary" : undefined}
+                >
+                  <ImStrikethrough />
+                </IconButton>
+              </div>
+              <hr className="my-1 border-default" />
+              <div className="p-2 space-y-1">
+                <TypographyItem
+                  label="Title"
+                  labelClass="text-lg font-bold"
+                  active={editor.isActive("heading", { level: 1 })}
                   onClick={() =>
                     editor.chain().focus().toggleHeading({ level: 1 }).run()
                   }
-                >
-                  Title
-                </button>
-              </Popover.Panel>
-            </Transition>
+                />
+                <TypographyItem
+                  label="Heading"
+                  labelClass="text-md font-bold"
+                  active={editor.isActive("heading", { level: 2 })}
+                  onClick={() =>
+                    editor.chain().focus().toggleHeading({ level: 2 }).run()
+                  }
+                />
+                <TypographyItem
+                  label="Subheading"
+                  labelClass="text-sm font-bold"
+                  active={editor.isActive("heading", { level: 3 })}
+                  onClick={() =>
+                    editor.chain().focus().toggleHeading({ level: 3 }).run()
+                  }
+                />
+                <TypographyItem
+                  label="Body"
+                  labelClass="text-sm"
+                  active={editor.isActive("paragraph")}
+                  onClick={() => editor.chain().focus().setParagraph().run()}
+                />
+                <TypographyItem
+                  label="&#8226; Bullet List"
+                  labelClass="text-sm"
+                  active={editor.isActive("bulletList")}
+                  onClick={() =>
+                    editor.chain().focus().toggleBulletList().run()
+                  }
+                />
+                <TypographyItem
+                  label="1. Numbered List"
+                  labelClass="text-sm"
+                  active={editor.isActive("orderedList")}
+                  onClick={() =>
+                    editor.chain().focus().toggleOrderedList().run()
+                  }
+                />
+                <TypographyItem
+                  label="Code"
+                  labelClass="text-sm"
+                  active={editor.isActive("codeBlock")}
+                  onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+                />
+                <TypographyItem
+                  label="Block quote"
+                  labelClass="text-sm"
+                  active={editor.isActive("blockquote")}
+                  onClick={() =>
+                    editor.chain().focus().toggleBlockquote().run()
+                  }
+                />
+              </div>
+            </Popover.Panel>
           </Popover>
         )}
       </div>
@@ -107,4 +146,33 @@ export const RichEditor = () => {
   );
 };
 
+interface TypographyItemProps {
+  label: string;
+  labelClass?: string;
+  active: boolean;
+  onClick: () => void;
+}
+const TypographyItem: React.FC<TypographyItemProps> = memo(
+  ({ active, label, onClick, labelClass }) => {
+    return (
+      <div>
+        <button
+          type="button"
+          className="relative flex rounded-md justify-start items-center w-full px-1 hover:bg-primary-300 dark:hover:bg-primary-300 dark:hover:text-default"
+          onClick={onClick}
+        >
+          <span
+            className={clsx(
+              active && "opacity-100",
+              "opacity-0 mx-2 transition-opacity"
+            )}
+          >
+            &#10003;
+          </span>
+          <span className={labelClass}>{label} </span>
+        </button>
+      </div>
+    );
+  }
+);
 export default RichEditor;
