bbe6eeb33c146f8f02f73b0d9557501739320c23
Tergel Munkhdelger <tergelm@gmail.com>
2021-05-24 09:56:47 +0800

    folder ui changes

--- components/Folders/FolderItem.tsx
+++ components/Folders/FolderItem.tsx
@@ -95,33 +95,41 @@ export const FolderItem: React.FC<FolderProps> = ({
             <span className="ml-2 text-sm line-clamp-1">{title}</span>
           </div>
           <div className="flex items-center justify-between">
-            <Menu as="div" className="relative inline-block text-left">
-              {({ open }) => (
-                <>
-                  <div className="flex items-center justify-center">
-                    <Menu.Button
-                      as={IconButton}
-                      size="sm"
-                      isRound
-                      aria-label="Menu"
-                      className="transition-opacity opacity-0 group-hover:opacity-100 px-0 text-md py-0"
-                    >
-                      <IoEllipsisHorizontalCircleSharp />
-                    </Menu.Button>
-                  </div>
+            {title !== "Recently Deleted" && (
+              <Menu as="div" className="relative inline-block text-left">
+                {({ open }) => (
+                  <>
+                    <div className="flex items-center justify-center">
+                      <Menu.Button className="transition-opacity opacity-0 group-hover:opacity-100 h-full inline-flex justify-center items-center rounded-full bg-gray-300 bg-opacity-0 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                        <IoEllipsisHorizontalCircleSharp fontSize={16} />
+                      </Menu.Button>
+                    </div>
 
-                  <Transition
-                    show={open}
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute z-50 left-0 w-24 mt-2 origin-top-right bg-paper rounded-md border dark:border-gray-600 border-opacity-40 border-gray-200　shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-text-primary">
-                      {isDeletable && (
+                    <Transition
+                      show={open}
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute z-50 left-0 w-24 mt-2 origin-top-right bg-paper rounded-md border dark:border-gray-600 border-opacity-40 border-gray-200　shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-text-primary">
+                        {isDeletable && (
+                          <div className="px-1 py-1">
+                            <Menu.Item
+                              as={Button}
+                              size="sm"
+                              variant="ghost"
+                              isFullWidth
+                              className="text-xs font-normal"
+                            >
+                              Delete
+                            </Menu.Item>
+                          </div>
+                        )}
+
                         <div className="px-1 py-1">
                           <Menu.Item
                             as={Button}
@@ -130,27 +138,16 @@ export const FolderItem: React.FC<FolderProps> = ({
                             isFullWidth
                             className="text-xs font-normal"
                           >
-                            Delete
+                            New folder
                           </Menu.Item>
                         </div>
-                      )}
+                      </Menu.Items>
+                    </Transition>
+                  </>
+                )}
+              </Menu>
+            )}
 
-                      <div className="px-1 py-1">
-                        <Menu.Item
-                          as={Button}
-                          size="sm"
-                          variant="ghost"
-                          isFullWidth
-                          className="text-xs font-normal"
-                        >
-                          New folder
-                        </Menu.Item>
-                      </div>
-                    </Menu.Items>
-                  </Transition>
-                </>
-              )}
-            </Menu>
             <span className="ml-2">{count}</span>
           </div>
         </div>
